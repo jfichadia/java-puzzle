2af2025ef54a76931bb9743c232e472ffa7357ee
Jay Fichadia <fichadia.jay@gmail.com>
2015-05-15 21:25:47 +0530

    Added eclipse project
    Change-Id: I1328e3c58c6ae565d458013fdc8fa05de6c7113c
    Signed-off-by: Jay Fichadia <fichadia.jay@gmail.com>

---
+++ java-puzzles/src/solution/SquareRootSolution.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package solution;
+
+/**
+ * @author Jay Fichadia (fichadia.jay@gmail.com)
+ *
+ */
+public class SquareRootSolution {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
